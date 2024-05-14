24d38a50a585576a6a525ee171cd0409d9249f18
muhammmad.ramdan <muhammad.ramdan@spesolution.com>
2024-05-14 14:08:38 +0700

    add seeder kota

--- app/Http/Requests/StoreRuangKelasRequest.php
+++ app/Http/Requests/StoreRuangKelasRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreRuangKelaRequest extends FormRequest
+class StoreRuangKelasRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
---
+++ database/seeders/KotaSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kota;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+
+class KotaSeeder extends Seeder
+{
+    public function run()
+    {
+        Kota::create([
+            'nama_kota' => 'Jakarta',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ],
+        [
+            'nama_kota' => 'Makasar',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ],
+        [
+            'nama_kota' => 'Bali',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
