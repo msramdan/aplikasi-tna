077dfc3580f5a13d69d507f7669563dbb456da61
farhanfdlhq <fadilahfarhan80@gmail.com>
2024-05-29 14:53:22 +0700

    menambah fitur permission

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,146 +11,54 @@
                 </a>
             </li>
 
-            <li class="nav-item">
-                <a class="nav-link menu-link" href="#sidebarMasterData" data-bs-toggle="collapse" role="button"
-                    aria-expanded="false" aria-controls="sidebarMasterData">
-                    <i class="fa fa-list"></i> <span data-key="t-forms">Master data</span>
-                </a>
-                <div class="collapse menu-dropdown" id="sidebarMasterData">
-                    <ul class="nav nav-sm flex-column">
-                        <li class="nav-item">
-                            <a href="{{ route('kompetensi.index') }}" class="nav-link" data-key="t-basic-elements">Kamus
-                                kompetensi</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('topik.index') }}" class="nav-link"
-                                data-key="t-basic-elements">Pembelajaran</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('kota.index') }}" class="nav-link" data-key="t-basic-elements">Daftar
-                                kota</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('lokasi.index') }}" class="nav-link"
-                                data-key="t-basic-elements">Lokasi</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('ruang-kelas.index') }}" class="nav-link"
-                                data-key="t-basic-elements">Ruang kelas</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('asrama.index') }}" class="nav-link"
-                                data-key="t-basic-elements">Asrama</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-
-            {{-- <li class="nav-item">
-                <a class="nav-link menu-link" href="#sidebarMultilevel" data-bs-toggle="collapse" role="button"
-                    aria-expanded="false" aria-controls="sidebarMultilevel">
-                    <i class="fa fa-tag"></i> <span data-key="t-multi-level">Setting Tagging</span>
-                </a>
-                <div class="collapse menu-dropdown" id="sidebarMultilevel">
-                    <ul class="nav nav-sm flex-column">
-
-                        <li class="nav-item">
-                            <a href="" class="nav-link" role="button"
-                                aria-expanded="false" aria-controls="sidebarApip" data-key="t-level-1.2"> Tag
-                                Pembelajaran & Kompetensi
-                            </a>
-                        </li>
-
-
-                        <li class="nav-item">
-                            <a href="#sidebarBpkp" class="nav-link" data-bs-toggle="collapse" role="button"
-                                aria-expanded="false" aria-controls="sidebarBpkp" data-key="t-level-1.2"> Tag
-                                Kompetensi - IK
-                            </a>
-                            <div class="collapse menu-dropdown" id="sidebarBpkp">
-                                <ul class="nav nav-sm flex-column">
-                                    <li class="nav-item">
-                                        <a href="#sidebarRenstra" class="nav-link" data-bs-toggle="collapse"
-                                            role="button" aria-expanded="false" aria-controls="sidebarRenstra"
-                                            data-key="t-level-2.2">
-                                            Renstra
-                                        </a>
-                                        <div class="collapse menu-dropdown" id="sidebarRenstra">
-                                            <ul class="nav nav-sm flex-column">
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link" data-key="t-level-3.1">Tag
-                                                        Pembelajaran - Kompetensi </a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link" data-key="t-level-3.2"> Tag
-                                                        Kompetensi - IK </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a href="#sidebarAPP" class="nav-link" data-bs-toggle="collapse" role="button"
-                                            aria-expanded="false" aria-controls="sidebarAPP" data-key="t-level-2.2">
-                                            APP
-                                        </a>
-                                        <div class="collapse menu-dropdown" id="sidebarAPP">
-                                            <ul class="nav nav-sm flex-column">
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link" data-key="t-level-3.1">Tag
-                                                        Pembelajaran - Kompetensi </a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link" data-key="t-level-3.2"> Tag
-                                                        Kompetensi - IK </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a href="#sidebarAPEP" class="nav-link" data-bs-toggle="collapse"
-                                            role="button" aria-expanded="false" aria-controls="sidebarAPEP"
-                                            data-key="t-level-2.2"> APEP
-                                        </a>
-                                        <div class="collapse menu-dropdown" id="sidebarAPEP">
-                                            <ul class="nav nav-sm flex-column">
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link" data-key="t-level-3.1">Tag
-                                                        Pembelajaran - Kompetensi </a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link" data-key="t-level-3.2"> Tag
-                                                        Kompetensi - IK </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="#sidebarApip" class="nav-link" data-bs-toggle="collapse" role="button"
-                                aria-expanded="false" aria-controls="sidebarApip" data-key="t-level-1.2"> APIP
-                            </a>
-                            <div class="collapse menu-dropdown" id="sidebarApip">
-                                <ul class="nav nav-sm flex-column">
-                                    <li class="nav-item">
-                                        <a href="#" class="nav-link" data-key="t-level-3.1">Tag
-                                            Pembelajaran - Kompetensi </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="#" class="nav-link" data-key="t-level-3.2"> Tag
-                                            Kompetensi - IK </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-
-                    </ul>
-                </div>
-            </li> --}}
+            @canany(['kompetensi view', 'topik view', 'kota view', 'lokasi view', 'ruang kelas view', 'asrama view'])
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#sidebarMasterData" data-bs-toggle="collapse" role="button"
+                        aria-expanded="false" aria-controls="sidebarMasterData">
+                        <i class="fa fa-list"></i> <span data-key="t-forms">Master data</span>
+                    </a>
+                    <div class="collapse menu-dropdown" id="sidebarMasterData">
+                        <ul class="nav nav-sm flex-column">
+                            @can(['kompetensi view'])
+                                <li class="nav-item">
+                                    <a href="{{ route('kompetensi.index') }}" class="nav-link" data-key="t-basic-elements">Kamus
+                                        kompetensi</a>
+                                </li>
+                            @endcan
+                            @can(['topik view'])
+                                <li class="nav-item">
+                                    <a href="{{ route('topik.index') }}" class="nav-link"
+                                        data-key="t-basic-elements">Pembelajaran</a>
+                                </li>
+                            @endcan
+                            @can(['kota view'])
+                                <li class="nav-item">
+                                    <a href="{{ route('kota.index') }}" class="nav-link" data-key="t-basic-elements">Daftar
+                                        kota</a>
+                                </li>
+                            @endcan
+                            @can(['lokasi view'])
+                                <li class="nav-item">
+                                    <a href="{{ route('lokasi.index') }}" class="nav-link"
+                                        data-key="t-basic-elements">Lokasi</a>
+                                </li>
+                            @endcan
+                            @can(['ruang kelas view'])
+                                <li class="nav-item">
+                                    <a href="{{ route('ruang-kelas.index') }}" class="nav-link"
+                                        data-key="t-basic-elements">Ruang kelas</a>
+                                </li>
+                            @endcan
+                            @can(['asrama view'])
+                                <li class="nav-item">
+                                    <a href="{{ route('asrama.index') }}" class="nav-link"
+                                        data-key="t-basic-elements">Asrama</a>
+                                </li>
+                            @endcan
+                        </ul>
+                    </div>
+                </li>
+            @endcanany
 
             <li class="nav-item">
                 <a class="nav-link menu-link" href="#sidebarMultilevel" data-bs-toggle="collapse" role="button"
@@ -202,11 +110,14 @@
                     </a>
                 </li>
             @endcan
-            <li class="nav-item">
-                <a class="nav-link menu-link" href="{{ route('reporting.index') }}">
-                    <i class="fa fa-book"></i> <span data-key="t-widgets">Reporting</span>
-                </a>
-            </li>
+
+            @can(['reporting view'])
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="{{ route('reporting.index') }}">
+                        <i class="fa fa-book"></i> <span data-key="t-widgets">Reporting</span>
+                    </a>
+                </li>
+            @endcan
 
             <li class="nav-item">
                 <a class="nav-link menu-link" href="#sidebarUtilities" data-bs-toggle="collapse" role="button"
