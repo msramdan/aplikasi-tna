ed177392f6ad35ecf6392b37f9836a217d73a16d
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-06-09 19:50:39 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -7,45 +7,59 @@ use App\Http\Controllers\{
     ProfileController,
     RoleAndPermissionController,
     ActivityLogController,
-    LocalizationController
+    AsramaController,
+    JadwalKapTahunanController,
+    KalenderPembelajaranController,
+    KompetensiController,
+    KotaController,
+    LocalizationController,
+    LokasiController,
+    ReportingController,
+    RuangKelasController,
+    SettingAppController,
+    TaggingPembelajaranKompetensiController,
+    TopikController
 };
 use App\Http\Controllers\Auth\OtpController;
 
-//route switch bahasa
 Route::get('/localization/{language}', [LocalizationController::class, 'switch'])->name('localization.switch');
+Route::get('/dashboard', function () {
+    return redirect()->route('dashboard');
+});
+
 Route::middleware(['auth', 'web'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', ProfileController::class)->name('profile');
     Route::resource('users', UserController::class);
     Route::resource('roles', RoleAndPermissionController::class);
-    Route::resource('setting-apps', App\Http\Controllers\SettingAppController::class);
-});
-Route::get('/dashboard', function () {
-    return redirect()->route('dashboard');
+    Route::resource('setting-apps', SettingAppController::class);
+
+    Route::resource('kompetensi', KompetensiController::class);
+    Route::controller(KompetensiController::class)->group(function () {
+        Route::get('/detailKompetensi', 'detailKompetensi')->name('detailKompetensi');
+        Route::get('/exportKompetensi', 'exportKompetensi')->name('exportKompetensi');
+        Route::post('/importKompetensi', 'importKompetensi')->name('importKompetensi');
+    });
+
+    Route::resource('kalender-pembelajaran', KalenderPembelajaranController::class);
+    Route::resource('reporting', ReportingController::class);
+    Route::resource('kota', KotaController::class);
+    Route::resource('lokasi', LokasiController::class);
+    Route::resource('ruang-kelas', RuangKelasController::class);
+    Route::resource('asrama', AsramaController::class);
+
+    Route::resource('topik', TopikController::class);
+    Route::controller(TopikController::class)->group(function () {
+        Route::get('/exportTopik', 'exportTopik')->name('exportTopik');
+        Route::post('/importTopik', 'importTopik')->name('importTopik');
+    });
+
+    Route::resource('jadwal-kap-tahunan', JadwalKapTahunanController::class);
+    Route::resource('tagging-pembelajaran-kompetensi', TaggingPembelajaranKompetensiController::class);
+
+    Route::controller(ActivityLogController::class)->group(function () {
+        Route::get('/activity-log', 'index')->name('activity-log.index');
+    });
 });
 
-Route::resource('kompetensi', App\Http\Controllers\KompetensiController::class)->middleware('auth');
-Route::controller(App\Http\Controllers\KompetensiController::class)->group(function () {
-    Route::get('/detailKompetensi', 'detailKompetensi')->name('detailKompetensi');
-    Route::get('/exportKompetensi', 'exportKompetensi')->name('exportKompetensi');
-    Route::post('/importKompetensi', 'importKompetensi')->name('importKompetensi');
-});
-
-Route::resource('kalender-pembelajaran', App\Http\Controllers\KalenderPembelajaranController::class)->middleware('auth');
-Route::resource('reporting', App\Http\Controllers\ReportingController::class)->middleware('auth');
-Route::resource('kota', App\Http\Controllers\KotaController::class)->middleware('auth');
-Route::resource('lokasi', App\Http\Controllers\LokasiController::class)->middleware('auth');
-Route::resource('ruang-kelas', App\Http\Controllers\RuangKelasController::class)->middleware('auth');
-Route::resource('asrama', App\Http\Controllers\AsramaController::class)->middleware('auth');
-Route::resource('topik', App\Http\Controllers\TopikController::class)->middleware('auth');
-Route::controller(App\Http\Controllers\TopikController::class)->group(function () {
-    Route::get('/exportTopik', 'exportTopik')->name('exportTopik');
-    Route::post('/importTopik', 'importTopik')->name('importTopik');
-});
-Route::resource('jadwal-kap-tahunan', App\Http\Controllers\JadwalKapTahunanController::class)->middleware('auth');
-Route::resource('tagging-pembelajaran-kompetensi', App\Http\Controllers\TaggingPembelajaranKompetensiController::class)->middleware('auth');
-// activity log
-Route::controller(ActivityLogController::class)->group(function () {
-    Route::get('/activity-log', 'index')->name('activity-log.index');
-});
 Route::post('/verify-otp', [OtpController::class, 'verifyOtp'])->name('verify-otp');
