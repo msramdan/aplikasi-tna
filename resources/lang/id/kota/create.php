2673b9887147262770fc4aa4e4a4cff22f79e5b1
farhanfdlhq <fadilahfarhan80@gmail.com>
2024-06-21 15:01:41 +0700

    feat: city translate

--- resources/lang/id/kota/create.php
+++ resources/lang/id/kota/create.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    '' => '',
+    'title' => 'Buat kota',
+    'kota' => 'Kota',
+    'dashboard' => 'Dasboard',
+    'create' => 'Buat',
+    'back' => 'Kembali',
+    'save' => 'Simpan',
 ];
