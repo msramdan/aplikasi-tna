ef611abe49157c3c2c734623b67a79ceaac4146e
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-06-21 10:14:51 +0700

    feat : update export kompetensi

--- resources/views/kompetensi/kamus_kompetensi_excel.blade.php
+++ resources/views/kompetensi/kamus_kompetensi_excel.blade.php
@@ -2,6 +2,9 @@
     <thead>
         <tr>
             <th style="background-color:#D3D3D3 ">{{ trans('kamus_kompetensi_excel.no') }}</th>
+            <th style="background-color:#D3D3D3 ">Kelompok besar</th>
+            <th style="background-color:#D3D3D3 ">Kategori</th>
+            <th style="background-color:#D3D3D3 ">Akademi</th>
             <th style="background-color:#D3D3D3 ">{{ trans('kamus_kompetensi_excel.nama_kompetensi') }}</th>
             <th style="background-color:#D3D3D3 ">{{ trans('kamus_kompetensi_excel.deskripsi_kompetensi') }}</th>
             <th style="background-color:#D3D3D3 ">{{ trans('kamus_kompetensi_excel.level') }}</th>
@@ -13,6 +16,9 @@
         @foreach ($data as $index => $row)
         <tr>
             <td>{{ $index + 1 }}</td>
+            <td>{{ $row->nama_kelompok_besar }}</td>
+            <td>{{ $row->nama_akademi }}</td>
+            <td>{{ $row->nama_kategori_kompetensi }}</td>
             <td>{{ $row->nama_kompetensi }}</td>
             <td>{{ $row->deskripsi_kompetensi }}</td>
             <td>{{ $row->level }}</td>
