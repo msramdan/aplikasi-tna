93c257c8e9bbccddcd8ff85b9475f5d21d981797
muhammmad.ramdan <muhammad.ramdan@spesolution.com>
2024-07-13 19:13:34 +0700

    update

---
+++ resources/views/pengajuan-kap/pdf.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .header-container {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .header {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .header img {
+            height: 55px;
+            margin-right: 10px;
+        }
+        .header .text {
+            display: inline-block;
+            text-align: left;
+        }
+        .header .address {
+            font-size: 12px;
+        }
+        .header .contact {
+            font-size: 10px;
+            font-style: italic;
+        }
+        .divider {
+            border-top: 2px solid #000; /* Adjusted to make the underline thicker */
+            margin-top: 5px;
+            width: 100%;
+        }
+        .title {
+            text-align: center;
+            font-size: 16px;
+            font-weight: bold;
+            margin-top: 10px;
+            margin-top: 20px
+        }
+    </style>
+</head>
+<body>
+    <div class="header-container">
+        <div class="header">
+            <img src="https://static.wikia.nocookie.net/logopedia/images/6/60/Badan_Pengawasan_Keuangan_dan_Pembangunan.png/revision/latest/scale-to-width-down/1200?cb=20190907043126" alt="BPKP Logo">
+            <div class="text">
+                <center>
+                    <strong>BADAN PENGAWASAN KEUANGAN DAN PEMBANGUNAN</strong><br>
+                    <strong>PUSAT PENDIDIKAN DAN PELATIHAN PENGAWASAN</strong>
+                    <div class="address">
+                        Jalan Beringin II, Pandansari, Ciawi, Kabupaten Bogor 16720<br>
+                        Telepon (0251) 824900-05, Faksimile (0251) 824986<br>
+                        E-mail: pusdiklatwas@bpkp.go.id, Website: pusdiklatwas.bpkp.go.id
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="title"><u>KERANGKA ACUAN PEMBELAJARAN</u></div>
+    </div>
+
+    <!-- Your PDF content goes here -->
+
+</body>
+</html>
