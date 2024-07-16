a033939edbc60431ceace77f93d6c559e85fdd95
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-07-16 09:20:11 +0700

    feat : add lagi if hit stara

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -35,96 +35,114 @@ class FortifyServiceProvider extends ServiceProvider
     public function boot()
     {
         Fortify::authenticateUsing(function (Request $request) {
-            $userId = session('otp_user_id');
-            session()->forget('otp_user_id');
-            if ($userId) {
-                Cache::forget('otp_' . $userId);
+            if (config('stara.is_hit')) {
+                $userId = session('otp_user_id');
+                session()->forget('otp_user_id');
+                if ($userId) {
+                    Cache::forget('otp_' . $userId);
+                }
+
+                $request->validate([
+                    'username' => 'required',
+                    'password' => 'required',
+                    'g-recaptcha-response' => 'required|captcha',
+                ]);
+
+                $endpointStara = config('stara.endpoint') . '/auth/login';
+                $response = Http::post($endpointStara, [
+                    'username' => $request->username,
+                    'password' => $request->password,
+                ]);
+
+                if ($response->successful()) {
+                    $data = $response->json();
+                    $user = User::where('user_nip', $data['data']['user_info']['user_nip'])->first();
+                    if (!$user) {
+                        $endpointUnitKerja = config('stara.endpoint') . '/unit-kerja';
+                        $unitKerjaResponse = Http::withToken($data['data']['token'])->get($endpointUnitKerja);
+                        if ($unitKerjaResponse->successful()) {
+                            $unitKerjaData = $unitKerjaResponse->json()['data'];
+                            $matchedUnit = collect($unitKerjaData)->firstWhere('nama_unit', $data['data']['user_info']['namaunit']);
+                            if ($matchedUnit) {
+                                $kode_unit = $matchedUnit['kode_unit'];
+                            } else {
+                                $kode_unit = '';
+                            }
+                        } else {
+                            dd('Failed to hit the unit-kerja endpoint');
+                        }
+
+                        $user = User::create([
+                            'user_nip' => $data['data']['user_info']['user_nip'],
+                            'name' => $data['data']['user_info']['name'],
+                            'phone' => $data['data']['user_info']['nomorhp'],
+                            'email' => $data['data']['user_info']['email'],
+                            'jabatan' => $data['data']['user_info']['jabatan'],
+                            'kode_unit' => $kode_unit,
+                            'nama_unit' => $data['data']['user_info']['namaunit']
+                        ]);
+                        $role = Role::where('id', 3)->first();
+                        if ($role) {
+                            $user->assignRole($role);
+                        } else {
+                            dd('Role not found');
+                        }
+                    }
+                    session(['api_token' => $data['data']['token']]);
+                    $now = Carbon::now();
+                    $now = Carbon::now();
+                    $jadwalData = DB::table('jadwal_kap_tahunan')
+                        ->where('tanggal_mulai', '<=', $now)
+                        ->where('tanggal_selesai', '>=', $now)
+                        ->first();
+
+                    if ($jadwalData) {
+                        session()->flash('login_success', true);
+                        session(['jadwal_kap_tahunan' => $jadwalData]);
+                    }
+
+                    if (env('IS_SEND_OTP', false)) {
+                        $otp = rand(100000, 999999);
+                        $otpExpiration = env('EXPIRED_OTP', 3);
+                        Cache::put('otp_' . $user->id, $otp, now()->addMinutes($otpExpiration));
+                        Mail::to($user->email)->send(new SendOtpMail($otp));
+                        session(['otp_user_id' => $user->id]);
+                        session(['otp_email' => $user->email]);
+                        return null;
+                    }
+
+                    if ($request->filled('remember')) {
+                        Auth::login($user, true);
+                    } else {
+                        Auth::login($user);
+                    }
+
+                    return $user;
+                }
+
+                throw ValidationException::withMessages([
+                    Fortify::username() => [trans('auth.failed')],
+                ]);
+            } else {
+                $user = User::where('name', $request->username)->first();
+                if (!$user) {
+                    $user = User::create([
+                        'user_nip' => generateRandomNip(),
+                        'name' =>  $request->username,
+                        'phone' =>  '-',
+                        'email' => generateRandomEmail(),
+                        'jabatan' =>  '-',
+                        'nama_unit' =>  '-'
+                    ]);
+                    $role = Role::where('id', 1)->first();
+                    if ($role) {
+                        $user->assignRole($role);
+                    } else {
+                        dd('Role not found');
+                    }
+                }
+                return $user;
             }
-
-            $request->validate([
-                'username' => 'required',
-                'password' => 'required',
-                'g-recaptcha-response' => 'required|captcha',
-            ]);
-
-            $user = User::where('name', $request->username)->first();
-            return $user;
-
-            // $endpointStara = config('stara.endpoint') . '/auth/login';
-            // $response = Http::post($endpointStara, [
-            //     'username' => $request->username,
-            //     'password' => $request->password,
-            // ]);
-
-            // if ($response->successful()) {
-            //     $data = $response->json();
-            //     $user = User::where('user_nip', $data['data']['user_info']['user_nip'])->first();
-            //     if (!$user) {
-            //         $endpointUnitKerja = config('stara.endpoint') . '/unit-kerja';
-            //         $unitKerjaResponse = Http::withToken($data['data']['token'])->get($endpointUnitKerja);
-            //         if ($unitKerjaResponse->successful()) {
-            //             $unitKerjaData = $unitKerjaResponse->json()['data'];
-            //             $matchedUnit = collect($unitKerjaData)->firstWhere('nama_unit', $data['data']['user_info']['namaunit']);
-            //             if ($matchedUnit) {
-            //                 $kode_unit = $matchedUnit['kode_unit'];
-            //             } else {
-            //                 $kode_unit = '';
-            //             }
-            //         } else {
-            //             dd('Failed to hit the unit-kerja endpoint');
-            //         }
-
-            //         $user = User::create([
-            //             'user_nip' => $data['data']['user_info']['user_nip'],
-            //             'name' => $data['data']['user_info']['name'],
-            //             'phone' => $data['data']['user_info']['nomorhp'],
-            //             'email' => $data['data']['user_info']['email'],
-            //             'jabatan' => $data['data']['user_info']['jabatan'],
-            //             'kode_unit' => $kode_unit,
-            //             'nama_unit' => $data['data']['user_info']['namaunit']
-            //         ]);
-            //         $role = Role::where('id', 3)->first();
-            //         if ($role) {
-            //             $user->assignRole($role);
-            //         } else {
-            //             dd('Role not found');
-            //         }
-            //     }
-            //     session(['api_token' => $data['data']['token']]);
-            //     $now = Carbon::now();
-            //     $now = Carbon::now();
-            //     $jadwalData = DB::table('jadwal_kap_tahunan')
-            //         ->where('tanggal_mulai', '<=', $now)
-            //         ->where('tanggal_selesai', '>=', $now)
-            //         ->first();
-
-            //     if ($jadwalData) {
-            //         session()->flash('login_success', true);
-            //         session(['jadwal_kap_tahunan' => $jadwalData]);
-            //     }
-
-            //     if (env('IS_SEND_OTP', false)) {
-            //         $otp = rand(100000, 999999);
-            //         $otpExpiration = env('EXPIRED_OTP', 3);
-            //         Cache::put('otp_' . $user->id, $otp, now()->addMinutes($otpExpiration));
-            //         Mail::to($user->email)->send(new SendOtpMail($otp));
-            //         session(['otp_user_id' => $user->id]);
-            //         session(['otp_email' => $user->email]);
-            //         return null;
-            //     }
-
-            //     if ($request->filled('remember')) {
-            //         Auth::login($user, true);
-            //     } else {
-            //         Auth::login($user);
-            //     }
-
-            //     return $user;
-            // }
-
-            throw ValidationException::withMessages([
-                Fortify::username() => [trans('auth.failed')],
-            ]);
         });
 
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
