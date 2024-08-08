9eeaaac0565b1ed90395d7a7dbb30f1269f5728e
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-08-08 18:54:52 +0700

    feat : update

--- app/Exports/ExportKalenderPembelajaran.php
+++ app/Exports/ExportKalenderPembelajaran.php
@@ -37,6 +37,7 @@ class ExportKalenderPembelajaran implements FromView, ShouldAutoSize, WithEvents
             ->leftJoin('pengajuan_kap', 'waktu_tempat.pengajuan_kap_id', '=', 'pengajuan_kap.id')
             ->leftJoin('lokasi', 'waktu_tempat.lokasi_id', '=', 'lokasi.id')
             ->leftJoin('topik', 'pengajuan_kap.topik_id', '=', 'topik.id')
+            ->leftJoin('kompetensi', 'pengajuan_kap.kompetensi_id', '=', 'kompetensi.id')
             ->where('pengajuan_kap.tahun', $this->tahun)
             ->where('pengajuan_kap.status_pengajuan', 'Approved');
 
@@ -51,13 +52,9 @@ class ExportKalenderPembelajaran implements FromView, ShouldAutoSize, WithEvents
         $data = $query->select(
             'waktu_tempat.tanggal_mulai as start',
             'waktu_tempat.tanggal_selesai as end',
-            'pengajuan_kap.kode_pembelajaran',
-            'pengajuan_kap.institusi_sumber',
-            'pengajuan_kap.jenis_program',
-            'pengajuan_kap.frekuensi_pelaksanaan',
-            'pengajuan_kap.tujuan_program_pembelajaran',
-            'lokasi.type as type_lokasi',
+            'pengajuan_kap.*',
             'lokasi.nama_lokasi',
+            'kompetensi.nama_kompetensi as nama_kompetensi',
             'topik.nama_topik'
         )->get();
 
@@ -70,7 +67,7 @@ class ExportKalenderPembelajaran implements FromView, ShouldAutoSize, WithEvents
     {
         return [
             AfterSheet::class    => function (AfterSheet $event) {
-                $cellRange = 'A1:I1';
+                $cellRange = 'A1:M1';
                 $event->sheet->getStyle($cellRange)->applyFromArray([
                     'borders' => [
                         'allBorders' => [
