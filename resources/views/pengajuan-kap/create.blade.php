fc29c38ee943deb03040f5759d90c6fdb6abcf7b
muhammmad.ramdan <muhammad.ramdan@spesolution.com>
2024-10-15 15:12:58 +0700

    update

--- resources/views/pengajuan-kap/create.blade.php
+++ resources/views/pengajuan-kap/create.blade.php
@@ -329,7 +329,7 @@
                         modalBody.empty();
 
                         var table =
-                            '<div class="table-responsive"><table class="table table-sm table-striped" style="font-size:14px"><thead><tr>';
+                            '<div class="table-responsive"><table id="indikatorTable" class="table table-sm table-striped" style="font-size:14px"><thead><tr>';
                         var tableBody = '<tbody>';
 
                         if (jenisProgram === 'Renstra') {
@@ -396,6 +396,14 @@
                         table += tableBody + '</tbody></table></div>';
                         modalBody.append(table);
 
+                        $('#indikatorTable').DataTable({
+                            responsive: true,
+                            paging: true,
+                            searching: true,
+                            ordering: true,
+                            info: true
+                        });
+
                         $('#indikatorModal').modal('show');
                         $('#loading-overlay').hide();
                         $('.pilihIndikator').click(function() {
@@ -434,10 +442,7 @@
                         var modalBody = $('#kompetensiModal .modal-body');
                         modalBody.empty();
                         var table =
-                            '<div class="table-responsive"><table class="table table-sm table-striped" style="font-size:14px"><thead><tr>';
-                        var tableBody = '<tbody>';
-                        var table =
-                            '<div class="table-responsive"><table class="table table-sm table-striped" style="font-size:14px"><thead><tr>';
+                            '<div class="table-responsive"><table id="kompetensiTable" class="table table-sm table-striped" style="font-size:14px"><thead><tr>';
                         var tableBody = '<tbody>';
 
                         // Update table headers to include the new columns
@@ -471,6 +476,15 @@
 
                         table += tableBody + '</tbody></table></div>';
                         modalBody.append(table);
+
+                        $('#kompetensiTable').DataTable({
+                            responsive: true,
+                            paging: true,
+                            searching: true,
+                            ordering: true,
+                            info: true
+                        });
+
                         $('#kompetensiModal').modal('show');
                         $('#loading-overlay').hide();
                     },
