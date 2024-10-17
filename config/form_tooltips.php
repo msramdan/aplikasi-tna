f4b63a3a396814cbc9bbf5d11c47d1d1d906a724
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-10-17 16:11:41 +0700

    update

--- config/form_tooltips.php
+++ config/form_tooltips.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'referensi_indikator_kinerja' => 'Ketikan Isian',
+    'referensi_indikator_kinerja' => 'Isi kolom ini dengan indikator kinerja utama yang diharapkan tercapai pada tahun 2025 sebagai hasil dari pembelajaran yang diusulkan. Kolom ini juga bersifat sementara menunggu kesiapan API indikato kinerjar 2025 dari aplikasi E-SAKIP. Jika sudah tersedia di E-SAKIP maka kolom ini akan dinonaktifkan. Contoh: “Peningkatan nilai koreksi PNBP sebesar 10% setelah pelaksanaan pelatihan, dengan target nilai koreksi meningkat dari Rp50 miliar menjadi Rp55 miliar pada tahun 2025.',
     'judul' => 'Ketikan Isian',
     'arahan_pimpinan' => 'Ketikan Isian',
     'tujuan_program_pembelajaran' => 'Ketikan Isian',
